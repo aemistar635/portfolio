a4e53b9d70b09b25784a0b2ffb8f98c5015402f7
muhammadaamir635 <muhammad.aamir@darvis.com>
2022-11-07 12:36:39 +0500

    fix about section for small screen

--- components/AboutSection.vue
+++ components/AboutSection.vue
@@ -11,31 +11,35 @@
           <div
             class="w-full mb-6 lg:mb-0 mx-auto relative bg-white text-center dark:bg-[#111111] px-6 rounded-[20px] mt-[180px] md:mt-[50px] lg:mt-0"
           >
-            <!-- profile image  -->
-            <div class="max-w-screen-lg mx-auto mt-5">
-              <img
-                class="w-[240px] absolute left-[50%] transform -translate-x-[50%] h-[240px] drop-shadow-xl mx-auto rounded-[20px] -mt-[140px]"
-                :src="require(`~/static/amir.jpeg`)"
-                alt="profile"
-              >
-              <!-- <img
+            <div class="flex flex-col">
+              <!-- profile image  -->
+              <div class="mx-auto mt-5">
+                <img
+                  class="mx-auto w-[240px] h-[240px] drop-shadow-xl rounded-[20px] w-full object-cover"
+                  :src="require(`~/static/amir.jpeg`)"
+                  alt="profile"
+                >
+                <!-- <img
                 src="https://bostamihtml.ibthemespro.com/images/about/avatar.jpg"
                 class="mx-auto w-[240px] h-[240px] drop-shadow-xl rounded-[20px] w-full object-cover"
                 alt="about"
               > -->
+              </div>
+              <div class="">
+                <h2 class="mt-6 mb-1 text-[26px] font-semibold dark:text-white">
+                  Muhammad Aamir
+                  <!-- Monalisa Ashley -->
+                </h2>
+                <h3
+                  class="mb-4 text-[#7B7B7B] inline-block dark:bg-[#1D1D1D] px-5 py-1.5 rounded-lg dark:text-[#A6A6A6]"
+                >
+                  Frontend Developer
+                  <!-- Ui/Ux Designer -->
+                </h3>
+              </div>
             </div>
-            <div class="pb-8">
-              <h2 class="mt-6 mb-1 text-[26px] font-semibold dark:text-white">
-                Muhammad Aamir
-                <!-- Monalisa Ashley -->
-              </h2>
-              <h3
-                class="mb-4 text-[#7B7B7B] inline-block dark:bg-[#1D1D1D] px-5 py-1.5 rounded-lg dark:text-[#A6A6A6]"
-              >
-                Frontend Developer
-                <!-- Ui/Ux Designer -->
-              </h3>
 
+            <div class="mt-4">
               <SocialContacts class="" />
 
               <!-- Contact info start -->
