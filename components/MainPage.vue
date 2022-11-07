8d04dd763877569edd0bbdef5de2f44a8b138e87
muhammadaamir635 <muhammad.aamir@darvis.com>
2022-11-07 15:51:34 +0500

    add side menu bar for mobile view

--- components/MainPage.vue
+++ components/MainPage.vue
@@ -9,28 +9,60 @@
       id="nav"
       class="min-h-screen w-full flex flex-col bg-light-custom dark:bg-dark-custom bg-no-repeat bg-center bg-cover"
     >
-      <div class="w-1/2 mx-auto mt-8 flex justify-between items-center">
-        <div class="text-rose-500 text-2xl font-bold">
-          logo
+      <div class="hidden lg:block">
+        <div class="w-1/2 mx-auto mt-8 flex justify-between items-center">
+          <div class="text-rose-500 text-2xl font-bold">
+            logo
+          </div>
+          <div
+            id="theme-toggler"
+            class="bg-rose-500 rounded-full flex justify-center items-center bg-white p-2 cursor-pointer text-white text-[16px]"
+          >
+            <i
+              v-if="!darkMode"
+              class="far fa-moon"
+              @click="darkMode = !darkMode"
+            />
+            <i v-else class="fas fa-sun" @click="darkMode = !darkMode" />
+          </div>
         </div>
-        <div
-          id="theme-toggler"
-          class="bg-rose-500 rounded-full flex justify-center items-center bg-white p-2 cursor-pointer text-white text-[16px]"
-        >
-          <i v-if="!darkMode" class="far fa-moon" @click="darkMode = !darkMode" />
-          <i v-else class="fas fa-sun" @click="darkMode = !darkMode" />
+      </div>
+
+      <div class="block lg:hidden">
+        <div class="section-bg">
+          <div class="w-full flex justify-between px-4">
+            <a href="/"> logo </a>
+            <div class="flex items-center">
+              <!-- dark and light mode toggle -->
+              <i
+                v-if="!darkMode"
+                class="far fa-moon"
+                @click="darkMode = !darkMode"
+              />
+              <i v-else class="fas fa-sun" @click="darkMode = !darkMode" />
+              <!-- mobile toggle button -->
+              <button id="menu-toggle" type="button" class="menu-toggle-btn">
+                <i
+                  id="menu-toggle-open-icon"
+                  class="fas text-xl"
+                  :class="!showMenu ? 'fa-bars' : 'fa-xmark'"
+                  @click="showMenu = !showMenu"
+                />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
 
       <div
-        class="mx-auto container grid grid-cols-12 md:gap-10 justify-between lg:mt-[220px]"
+        class="mx-auto container grid grid-cols-12 md:gap-10 justify-between lg:mt-[200px]"
       >
         <!-- sidebar personal info -->
         <Sidebar />
         <!-- sidebar info end -->
 
         <div class="col-span-12 lg:col-span-8">
-          <Navbar class="lg:hidden block" />
+          <Navbar class="lg:hidden block" :open-menu="showMenu" />
 
           <!-- {{--about section--}} -->
           <AboutSection />
@@ -50,9 +82,37 @@ export default {
   name: 'MainPage',
   data () {
     return {
-      darkMode: false
+      darkMode: false,
+      showMenu: false
     }
   }
 }
 </script>
-<style></style>
+<style>
+.section-bg {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  --tw-bg-opacity: 1;
+  background-color: rgb(243 246 246 / var(--tw-bg-opacity));
+  padding-top: 1.25rem;
+  padding-bottom: 1.25rem;
+}
+.menu-toggle-btn {
+  margin-left: 0.75rem;
+  display: flex;
+  height: 40px;
+  width: 40px;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+  border-radius: 9999px;
+  --tw-bg-opacity: 1;
+  background-color: rgb(255 255 255 / var(--tw-bg-opacity));
+  --tw-text-opacity: 1;
+  color: rgb(0 0 0 / var(--tw-text-opacity));
+  transition-property: all;
+  transition-duration: 300ms;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+}
+</style>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,29 +1,56 @@
 <template>
   <header
-    class="sticky top-0 z-10 lg:relative mt-5 lg:border block py-[25px] lg:rounded-[16px] bg-gray-100/75 lg:bg-white dark:bg-[#111111]"
+    class="top-0 z-10 lg:relative mt-5 lg:border block py-[25px] lg:rounded-[16px] bg-gray-100/75 lg:bg-white dark:bg-[#111111]"
   >
     <nav>
-      <ul class="flex justify-center items-center flex-wrap 2xl:gap-0 gap-6">
-        <li v-for="nav in navBar" :key="nav.name">
-          <a
-            class="menu-item bg-stone-100"
-            :class="nav.active ? 'active' : ''"
-            :href="nav.href"
-            @click="activeTab(nav.name)"
-          >
-            <span class="text-xl mb-1">
-              <i :class="nav.icon" />
-            </span>
-            {{ nav.name }}
-          </a>
-        </li>
-      </ul>
+      <div class="hidden lg:block">
+        <ul class="flex justify-center items-center flex-wrap 2xl:gap-0 gap-6">
+          <li v-for="nav in navBar" :key="nav.name">
+            <a
+              class="menu-item bg-stone-100"
+              :class="nav.active ? 'active' : ''"
+              :href="nav.href"
+              @click="activeTab(nav.name)"
+            >
+              <span class="text-xl mb-1">
+                <i :class="nav.icon" />
+              </span>
+              {{ nav.name }}
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div v-if="openMenu" class="lg:hidden block">
+        <ul
+          class="block w-full absolute left-0 top-[68px] rounded-b-[20px] shadow-md z-[22222222222222] w-full bg-white dark:bg-[#1d1d1d]"
+        >
+          <li v-for="nav in navBar" :key="nav.name">
+            <a
+              class="mobile-menu-items"
+              :class="nav.active ? 'mobile-menu-items-active' : ''"
+              :href="nav.href"
+              @click="activeTab(nav.name)"
+            >
+              <span class="mr-2 text-xl">
+                <i :class="nav.icon" />
+              </span>
+              {{ nav.name }}
+            </a>
+          </li>
+        </ul>
+      </div>
     </nav>
   </header>
 </template>
 <script>
 export default {
   name: 'NavbarComponent',
+  props: {
+    openMenu: {
+      type: Boolean,
+      default: false
+    }
+  },
   data () {
     return {
       navBar: [
@@ -91,5 +118,54 @@ export default {
 .active {
   background: #fa5252;
   color: #fff;
+}
+.shadow-md {
+  --tw-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+  --tw-shadow-colored: 0 4px 6px -1px var(--tw-shadow-color),
+    0 2px 4px -2px var(--tw-shadow-color);
+  box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000),
+    var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
+}
+.mobile-menu-items {
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  padding-top: 0.625rem;
+  padding-bottom: 0.625rem;
+  padding-left: 1rem;
+  font-family: Poppins, sans-serif;
+  font-size: 0.8125rem;
+  font-weight: 500;
+  --tw-text-opacity: 1;
+  color: rgb(68 86 108 / var(--tw-text-opacity));
+  transition-property: color, background-color, border-color, fill, stroke,
+    -webkit-text-decoration-color;
+  transition-property: color, background-color, border-color,
+    text-decoration-color, fill, stroke;
+  transition-property: color, background-color, border-color,
+    text-decoration-color, fill, stroke, -webkit-text-decoration-color;
+  transition-duration: 300ms;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+}
+.mobile-menu-items-active {
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  padding-top: 0.625rem;
+  padding-bottom: 0.625rem;
+  padding-left: 1rem;
+  font-family: Poppins, sans-serif;
+  font-size: 0.8125rem;
+  font-weight: 500;
+  --tw-text-opacity: 1;
+  color: rgb(250 82 82 / var(--tw-text-opacity));
+  transition-property: color, background-color, border-color, fill, stroke,
+    -webkit-text-decoration-color;
+  transition-property: color, background-color, border-color,
+    text-decoration-color, fill, stroke;
+  transition-property: color, background-color, border-color,
+    text-decoration-color, fill, stroke, -webkit-text-decoration-color;
+  transition-duration: 300ms;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
 }
 </style>
