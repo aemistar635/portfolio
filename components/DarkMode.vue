9a04bd4ac2a50f3ac66a9bd494efb422a3e128db
muhammadaamir635 <muhammad.aamir@darvis.com>
2023-10-17 11:24:32 +0500

    fixes

--- components/DarkMode.vue
+++ components/DarkMode.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     id="theme-toggler"
-    class="bg-rose-500 rounded-full flex justify-center items-center bg-white p-2 cursor-pointer text-white xl:text-[16px] lg:text-[10px] text-[16px]"
+    class="bg-rose-500 rounded-full flex justify-center items-center p-2 cursor-pointer text-white xl:text-[16px] lg:text-[10px] text-[16px]"
   >
     <i v-if="!darkMode" class="far fa-moon" @click="changeMode(true)" />
     <i v-else class="fas fa-sun" @click="changeMode(false)" />
