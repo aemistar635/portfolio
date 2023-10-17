9396a764e440b41456b66b894ef6923688e97581
muhammadaamir635 <muhammad.aamir@darvis.com>
2023-10-17 11:41:06 +0500

    dark mode button

--- components/DarkMode.vue
+++ components/DarkMode.vue
@@ -1,10 +1,11 @@
 <template>
   <div
     id="theme-toggler"
-    class="bg-rose-500 rounded-full flex justify-center items-center p-2 cursor-pointer text-white xl:text-[16px] lg:text-[10px] text-[16px]"
-  >
-    <i v-if="!darkMode" class="far fa-moon" @click="changeMode(true)" />
-    <i v-else class="fas fa-sun" @click="changeMode(false)" />
+    class="bg-rose-500 rounded-full flex justify-center items-center p-4 cursor-pointer text-white xl:text-[16px] lg:text-[10px] text-[20px]"
+    @click="changeMode(!darkMode)"
+    >
+    <i v-if="!darkMode" class="far fa-moon" />
+    <i v-else class="fas fa-sun" />
   </div>
 </template>
 <script>
